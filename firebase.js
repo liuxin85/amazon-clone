695ed74bd0fbc42e8bad79fb5abe4800d45339a5
liuxin85 <liuxin85@gmail.com>
2021-12-28 00:49:42 +0800

    day 3

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCK9LsJMSJKRB--m4dbcvXo5Wyt_mPXgsQ",
+  authDomain: "clone-93fb5.firebaseapp.com",
+  projectId: "clone-93fb5",
+  storageBucket: "clone-93fb5.appspot.com",
+  messagingSenderId: "561394429708",
+  appId: "1:561394429708:web:1b63204e4b5a5c4b762329",
+};
